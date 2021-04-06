bc6be4e5bd1a78b8f5e09da95f91668a33fb1283
Aart den Hoed <adh.code@gmail.com>
2021-04-06 22:28:58 +0200

    Initial

---
+++ Forttrantest/Forttrantest.f90
@@ -0,0 +1,25 @@
+!  Forttrantest.f90 
+!
+!  FUNCTIONS:
+!  Forttrantest - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Forttrantest
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Forttrantest
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Forttrantest
+    print *, 'Hello World'
+
+    end program Forttrantest
+
